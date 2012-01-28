f6ce186e2caa3c036026f5ff2af5b549a2e3a245
Shura Scherban <shura.71@gmail.com>
2012-01-28 20:38:14 +0200

    Make compatible with RedCarpet 2.1.0

--- app/helpers/rails_markitup/markitup_helper.rb
+++ app/helpers/rails_markitup/markitup_helper.rb
@@ -1,6 +1,6 @@
 module RailsMarkitup::MarkitupHelper
   def markdown(text, style="bright")
-     options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis, :fenced_code, :gh_blockcode]
-     Redcarpet.new(text, *options).to_html.html_safe
-   end
+    options = {:hard_wrap => true, :filter_html => true, :autolink => true, :no_intraemphasis => true, :fenced_code => true, :gh_blockcode => true}
+    Redcarpet::Markdown.new(Redcarpet::Render::HTML,options).render(text).html_safe
+  end
 end
